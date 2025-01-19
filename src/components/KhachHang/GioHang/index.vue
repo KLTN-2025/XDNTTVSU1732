055c53d3100c3563ace86e15bfa5903f7611b95d
Quốc Sang <sang0882k3@gmail.com>
2025-01-19 22:55:35 +0700

    thanh_toan

--- src/components/KhachHang/GioHang/index.vue
+++ src/components/KhachHang/GioHang/index.vue
@@ -45,7 +45,8 @@
                                         {{ index + 1 }}
                                     </th>
                                     <td class="text-center text-nowrap align-middle">
-                                        <img :src="value.hinh_anh" alt="" class="img-fluid" style="width: 100%; max-height: 80px; object-fit: contain;" />
+                                        <img :src="value.hinh_anh" alt="" class="img-fluid"
+                                            style="width: 100%; max-height: 80px; object-fit: contain;" />
                                     </td>
                                     <td class="text-wrap align-middle" style="width: 300px;">{{ value.ten_sach }}</td>
                                     <td style="width: 140px" class="align-middle">
@@ -88,8 +89,77 @@
                         <button class="btn btn-outline-danger me-3">
                             Tổng tiền: {{ formatVND(tong_tien) }}
                         </button>
-                        <button class="btn btn-success">Thanh Toán</button>
+                        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#muaHangModal">Thanh Toán</button>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="muaHangModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Xác Nhận Mua Hàng</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table mb-0 table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="text-center text-nowrap">#</th>
+                                <th class="text-center text-nowrap">Tên Sách</th>
+                                <th class="text-center text-nowrap">Số Lượng</th>
+                                <th class="text-center text-nowrap">Đơn Giá</th>
+                                <th class="text-center text-nowrap">Thành Tiền</th>
+                                <th class="text-center text-nowrap">Ghi Chú</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <template v-for="(value, index) in list_gio_hang" :key="index">
+                                <tr v-if="value.chon_sp == true">
+                                    <td class="text-center align-middle" style="width: 10px;">
+                                        {{ index + 1 }}
+                                    </td>
+                                    <td class="align-middle" style="width: 400px;">{{ value.ten_sach }}</td>
+                                    <td class="text-center align-middle">{{ value.so_luong }}</td>
+                                    <td class="text-end align-middle">{{ formatVND(value.don_gia) }}</td>
+                                    <td class="text-end align-middle">{{ formatVND(value.thanh_tien) }}
+                                    </td>
+                                    <td class="text-center align-middle" style="width: 350px;">
+                                        {{ value.ghi_chu }}
+                                    </td>
+                                </tr>
+                            </template>
+                            <tr>
+                                <th colspan="3" class="align-middle">
+                                    <select v-model="id_dia_chi" class="form-select">
+                                        <template v-for="(value, index) in list_dia_chi" :key="index">
+                                            <option value="">-- Vui Lòng Chọn Địa Chỉ Giao Hàng --</option>
+                                            <option v-bind:value="value.id">
+                                                {{ value.ten_nguoi_nhan }} - {{ value.so_dien_thoai }} - {{
+                                                value.dia_chi }}
+                                            </option>
+                                        </template>
+                                    </select>
+                                </th>
+                                 <th colspan="2" class="align-middle">
+                                        <select v-model="phuong_thuc" class="form-select">
+                                            <option value="">-- Chọn Kiểu Thanh Toán --</option>
+                                            <option value="0">Thanh toán khi nhận hàng</option>
+                                            <option value="1">Thanh toán qua mã QR</option>
+                                        </select>
+                                </th>
+                                <th colspan="2" class="align-middle text-nowrap text-end text-danger">
+                                    Tổng Tiền: {{ formatVND(tong_tien) }}
+                                </th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button v-on:click="muaHang()" type="button" class="btn btn-primary" data-bs-dismiss="modal">Xác Nhận Mua Hàng</button>
                 </div>
             </div>
         </div>
@@ -103,12 +173,33 @@ export default {
         return {
             list_gio_hang: [],
             tong_tien: 0,
+            list_dia_chi: [],
+            id_dia_chi: "",
+            phuong_thuc : ""
         };
     },
     mounted() {
         this.layDataGioHang();
+        this.loadDataDiaChi()
     },
     methods: {
+        loadDataDiaChi() {
+            axios
+                .get("http://127.0.0.1:8000/api/khach-hang/dia-chi/data", {
+                    headers: {
+                        Authorization: 'Bearer ' + localStorage.getItem("token_khach_hang")
+                    }
+                })
+                .then((res) => {
+                        this.list_dia_chi = res.data.data;
+                })
+                .catch((res) => {
+                    var list_error = Object.values(res.response.data.errors);
+                    list_error.forEach((v, k) => {
+                        this.$toast.error(v[0]);
+                    });
+                });
+        },
         tinhTongTien() {
             let dang_chon = 0;
             this.list_gio_hang.forEach((value, index) => {
@@ -133,15 +224,36 @@ export default {
                 })
                 .then((res) => {
                     this.list_gio_hang = res.data.data;
-                    // this.list_gio_hang.forEach((value, key) => {
-                    //     if (value.id == this.$route.params.id_chi_tiet) {
-                    //         value.dang_chon = 1;
-                    //     }
-                    // });
-                    // this.list_gio_hang = [...this.list_gio_hang];
-                    // this.tinhTongTien();
                 });
         },
+        muaHang() {
+			var list_chon = [];
+			this.list_gio_hang.forEach((value, key) => {
+				if (value.chon_sp && value.chon_sp == true) {
+					list_chon.push(value);
+				}
+			});
+			var payload = {
+				'id_dia_chi': this.id_dia_chi,
+				'ds_mua_sp': list_chon,
+                'phuong_thuc': this.phuong_thuc
+			};
+			axios
+				.post('http://127.0.0.1:8000/api/khach-hang/mua-hang', payload, {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token_khach_hang"),
+                    },
+                })
+				.then((res) => {
+					// if (res.data.status) {
+					// 	this.$toast.success('Thông báo<br>' + res.data.message);
+					// 	this.tong_tien = 0,
+					// 	this.layDataGioHang();
+					// } else {
+					// 	this.$toast.error('Thông báo<br>' + res.data.message);
+					// }
+				});
+		},
         capNhat(payload) {
             axios
                 .post("http://127.0.0.1:8000/api/khach-hang/gio-hang/update", payload, {
